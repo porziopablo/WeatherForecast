797ec6d4481dde9122a4a72bed36f48de00c6ceb
Pablo Porzio <pabloporzio97@gmail.com>
2024-10-23 15:55:59 -0300

    Initial Commit

---
+++ WeatherForecast/WeatherForecastApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherForecastApp.swift
+//  WeatherForecast
+//
+//  Created by Pablo Porzio (Modak) on 23/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct WeatherForecastApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
